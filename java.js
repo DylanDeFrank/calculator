0fa3ac8ff4a26456030683f2fafb14b89d89beb3
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-27 14:54:12 -0700

    Create pages and basic functions of calc math

---
+++ java.js
@@ -0,0 +1,17 @@
+const btn = document.createElement('button')
+
+
+
+
+function add (a, b) {
+    return a + b
+}
+function subtract (a, b) {
+    return a - b
+}
+function multiply (a, b) {
+    return a * b
+}
+function divide (a, b) {
+    return a / b
+}
