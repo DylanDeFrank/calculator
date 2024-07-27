ea95cd3f0012aa350334e6a72e9897a073109746
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-27 15:05:49 -0700

    For loop which creates buttons for each number

--- java.js
+++ java.js
@@ -1,5 +1,15 @@
-const btn = document.createElement('button')
+const divNumBtns = document.querySelector('div.numbers')
 
+function addBtns () {
+    for(i = 0; i <= 9; i++) {
+        const btn = document.createElement('button')
+        btn.textContent = i
+        divNumBtns.appendChild(btn)
+
+    }
+}
+
+addBtns()
 
 
 
