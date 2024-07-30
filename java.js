f79c93a32ac7dbf0e11f84c5d439f8ba3648db8b
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-29 23:29:15 -0700

    Create function that splits up calculator display
    I took the whole string displayed by the calculator and split it into
    each element. I then found the index of where the operator was. Every
    number before that was placed into one variable, every number after
    placed into another. I now end up with two number variables and an
    operator variable.

--- java.js
+++ java.js
@@ -2,6 +2,7 @@ const divNumBtns = document.querySelector('div.numbers')
 const operators = ['+', '-', '*', '/', '=', 'clear' ] 
 const divDisplay = document.querySelector('div.display')
 
+
 function addBtns () {
     for(i = 0; i <= 9; i++) {
         const btn = document.createElement('button')
@@ -12,6 +13,7 @@ function addBtns () {
     for(let operator of operators) {
         const btn = document.createElement('button')
         btn.textContent = operator
+        btn.id = operator
         divNumBtns.appendChild(btn)
     }}
 addBtns()
@@ -20,13 +22,34 @@ const buttons = document.querySelectorAll('button')
 
 buttons.forEach ((button) => {
     button.addEventListener('click', () => {
-        divDisplay.textContent = button.textContent
-       
+        let input = button.textContent
+        divDisplay.textContent += input 
+        displayNum = divDisplay.textContent
+      })
+       })
+function displaySplit() {
+       let variableSplit = displayNum.split('')
+       for(let symbol of operators) {
+            for(let variable of variableSplit) {
+        if (variable == symbol ) {
+            split = variableSplit.indexOf(variable)
+            operator = variable
 
-    })
-})
+        }}}
+       for(let variable of variableSplit) {
+        if (variableSplit.indexOf(variable) < split) {
+           firstNum += variable
+        } else if (variableSplit.indexOf(variable) > split) {
+            secondNum += variable
+        }
+       }
+       console.log(firstNum)
+       console.log(secondNum)
+       console.log(operator)
+    }   
 
-
+    const equals = document.querySelector('#=')
+    equals.textContent = YOO 
 function add (a, b) {
     return a + b
 }
@@ -39,3 +62,10 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b
 }
+
+let split = 0
+let displayNum = 0
+let firstNum = ''
+let operator = ''
+let secondNum = ''
+function operate (num, op, num) {}
